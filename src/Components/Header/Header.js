50d7ee7ed585a155a8bdcbbf33717737da4935c1
Maisha589 <maisharahman589@gmail.com>
2022-04-07 01:04:26 +0600

    header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import "./Header.css"
 
 const Header = () => {
     return (
-        <div>
+        <div className='header'>
             <h1>Fish World</h1>
+            <p><small>A beautiful fish can make your day</small></p>
         </div>
     );
 };
---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>Here we can see fish.</h2>
+        </div>
+    );
+};
+
+export default Shop;
